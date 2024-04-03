59b1d9f8065ab8e5da1eca02b10a07d53ae5ce56
Ploskarev Vadim <vvploskarev@mail.ru>
2024-04-03 03:32:47 +0300

    add RemoveRow for DPLL alg

--- src/Program.cs
+++ src/Program.cs
@@ -10,6 +10,26 @@ public class Clause{
         ints = ints1;
     }
 
+    //Method to remove row (if we have unit clause or we can drop this line according  
+    //to DPLL algorithm)
+    public void RemoveRow(int index){
+        if (index<0 || index>this.rows){
+            return;
+        }
+        int[,] new_ints = new int[this.rows-1,this.column];
+        for (int i=0; i<rows; i++){
+            if (i!=index){
+                int[] n_int = new int[rows];
+                for (int j=0; j<column;j++){
+                    new_ints[i,j] = ints[i,j];
+                }
+            }
+        }
+        this.ints = new_ints; 
+    }
+
+
+
 }
 class Program
 {
@@ -44,8 +64,21 @@ class Program
         return clause;
     }
     
+    public static string SAT(Clause clause){
+        List<int> pos_num = new List<int>();
+        List<int> neg_num = new List<int>();
+
+        return "";
+    }
+    
+    public static int FindUnitClause(int[,] ints){
+        return 0;
+    }
+    
     static void Main(string[] args)
     {
+        string path = @"C:\Users\VLADIMIR\Desktop\My_SAT_solver\test.txt";
         Clause clause = GetVars(path);
     }
 }
+
