f2ccdac231ca501ca497d179fdb49903411d7de1
Vadim PLoskarev <vvploskarev@mail.ru>
2024-04-23 08:57:12 +0300

    add README

---
+++ README.md
@@ -0,0 +1,39 @@
+# SAT Solver
+
+## What is it
+
+This project provides a SAT solver.
+
+## Getting Started
+
+### Build
+
+1. Firstly, clone this repository:
+
+```bash
+git clone https://github.com/ploskiy-2/SAT_Solver.git
+```
+
+2. Then, navigate to the project directory:
+
+```bash
+cd SAT_Solver/src
+```
+
+3. Build the project:
+
+```bash
+dotnet build -c Release
+```
+
+4. Finally, run this script to run tests:
+
+```bash
+python3 testall.py
+```
+
+5. Run the solver against a file in DIMACS format:
+
+```bash
+dotnet run <path_to_file>
+```
--- src/test.py
+++ src/test.py
@@ -7,59 +7,63 @@ unsat_msg = 's UNSATISFIABLE'
 
 in_file_name = sys.argv[1]
 result = subprocess.run(
-    ['dotnet', 'run', in_file_name],
+    ['dotnet', 'run', '--configuration', 'Release', in_file_name],
     capture_output=True,
     universal_newlines=True
-)
+    )
 
 tmp = tempfile.NamedTemporaryFile()
 with open(tmp.name, 'w') as f:
     f.write(result.stdout)
-tmp.flush()
+    f.seek(0)
+
 with open(tmp.name, 'r') as f:
-    status = f.readline().strip() #/tmp/tmpj5db4cmy:20: expected number
-    if status == unsat_msg: 
+    status = f.readline().strip('\n')
+
+    if (status == unsat_msg):
         picosat_status = subprocess.run(
             ['picosat', in_file_name],
             capture_output=True,
             universal_newlines=True
-        )
-        if picosat_status.stdout.strip() == unsat_msg:
+            )
+        if (picosat_status.stdout.strip('\n') == unsat_msg):
             print(f'{in_file_name}: PASSED')
             sys.exit(0)
         else:
             print(f'{in_file_name}: FAILED')
             sys.exit(1)
     else:
-        solution = f.readline().strip()[2:]
-        #print(solution)
+        solution = f.readline().strip('\n')[2:]
         tmp_in_file = tempfile.NamedTemporaryFile()
+
+        # copy from in_file_name to tmp file,
+        # change header and add solution literals as new clauses
         with open(tmp_in_file.name, 'w') as f_in_tmp:
             with open(in_file_name, 'r') as f_in:
                 for line in f_in:
-                    if line.startswith('c'):
-                        continue
-                    elif line.startswith('p'):
+                    if line[0] == 'p':
                         splitted = line.split()
                         new_line = ' '.join(splitted[:3]) + ' ' + \
                             str(int(splitted[2]) + int(splitted[3]))
                     else:
-                        new_line = line.strip()
+                        new_line = line.strip('\n')
                     f_in_tmp.write(new_line + '\n')
+
+            # convert solution to clauses
             for literal in solution.split():
                 if literal == '0':
                     break
                 f_in_tmp.write(literal + ' 0\n')
-        tmp_in_file.flush()
+
         picosat_status = subprocess.run(
-            ['picosat', tmp_in_file.name],
+            ['picosat', f_in_tmp.name],
             capture_output=True,
             universal_newlines=True
-        )
-        if picosat_status.stdout.strip().split('\n')[0] == sat_msg:
+            )
+
+        if (picosat_status.stdout.split('\n')[0] == sat_msg):
             print(f'{in_file_name}: PASSED')
             sys.exit(0)
         else:
             print(f'{in_file_name}: FAILED')
-            #print(picosat_status.stdout.strip().split('\n')[0])
             sys.exit(1)
